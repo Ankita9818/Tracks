24df539f132be1bfbc55fed152879b113ecea4fc
Ankita9818 <ankita.dixit@vinsol.com>
2018-01-31 17:53:50 +0530

    Exercise - Program from command line committed

---
+++ Advanced_Ruby-track/Exercise2-Dynamic_class/dynamic_class.rb
@@ -0,0 +1,31 @@
+# Class DynamicClass
+class DynamicClass
+  attr_reader :new_class_name, :class_obj
+  def initialize(class_name)
+    @new_class_name = class_name
+    @class_obj = Object.const_set(new_class_name, Class.new)
+  end
+
+  def def_method(method_name, method_code)
+    class_obj.class_eval do
+      define_method(method_name) do
+        instance_eval method_code
+      end
+    end
+    "--- Result ---\nHello, Your class #{new_class_name} with method #{method_name} is ready. Calling: #{new_class_name}.new.#{method_name}:"
+  end
+
+  def call(method_name)
+    class_obj.new.send method_name
+  end
+end
+
+print 'Please enter the class name: '
+class_name = gets.chomp
+print 'Please enter the method name you wish to define: '
+method_name = gets.chomp
+print 'Please enter the method\'s code:'
+method_code = gets.chomp
+my_class = DynamicClass.new(class_name)
+puts my_class.def_method(method_name, method_code)
+puts my_class.call(method_name)
