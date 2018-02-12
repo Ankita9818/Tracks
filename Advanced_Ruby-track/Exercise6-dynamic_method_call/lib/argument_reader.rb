65f85b48065714f0bb797386ff5554e6cf8939ad
Ankita9818 <ankita.dixit@vinsol.com>
2018-02-12 17:40:47 +0530

    rearranged print statement

--- Advanced_Ruby-track/Exercise6-dynamic_method_call/lib/argument_reader.rb
+++ Advanced_Ruby-track/Exercise6-dynamic_method_call/lib/argument_reader.rb
@@ -8,21 +8,22 @@ class ArgumentReader
 
   def get_params(string, method_name)
     string.method(method_name.to_sym).parameters.each do |param|
-      print "Please enter the #{param[0] =~ REQUIRED_PARAM ? 'required' : 'optional'} argument #{param[1]} : "
       if param[0] =~ REQUIRED_PARAM
-        get_required_params
+        get_required_params(param[1])
       else
-        get_optional_params
+        get_optional_params(param[1])
       end
     end
     arguments
   end
 
-  def get_required_params
+  def get_required_params(parameter_name)
+    print "Please enter the required argument #{parameter_name} : "
     arguments << gets.chomp
   end
 
-  def get_optional_params
+  def get_optional_params(parameter_name)
+    print "Please enter the optional argument #{parameter_name} : "
     arg = gets.chomp
     arguments <<  arg if arg != ""
   end
