f0772291e2aa388cef204758d97663e76742e8ff
Ankita9818 <ankita.dixit@vinsol.com>
2018-02-23 16:06:59 +0530

    removed string usage

--- Advanced_Ruby-track/Exercise10_cattr_accessor/bin/main.rb
+++ Advanced_Ruby-track/Exercise10_cattr_accessor/bin/main.rb
@@ -2,9 +2,13 @@ require_relative '../lib/person'
 require_relative '../lib/male'
 
 begin
-  Person.new.hair_colors = [:ginger]
+  p Male.hair_colors
+  p Person.new.hair_colors
   p Person.hair_colors
+  p Person.new.hair_colors
+  Person.new.hair_colors = :pink_blue
   p Person.hair_colors
+  p Person.new.hair_colors
   Person.hair_colors = [:brown, :black, :blonde, :red]
   p Person.hair_colors
   p Person.new.hair_colors
--- Advanced_Ruby-track/Exercise10_cattr_accessor/lib/cattr_accessor.rb
+++ Advanced_Ruby-track/Exercise10_cattr_accessor/lib/cattr_accessor.rb
@@ -3,50 +3,65 @@ require_relative 'invalid_option_error'
 # Class Class
 class Class
   def cattr_accessor(*args)
-    options = args.last.is_a?(::Hash) ? args.pop : {}
+    options = extract_options(*args)
+    validate_options(options)
+    create_cattr_methods(*args, options)
+  end
+
+  private
+
+  def extract_options(*args)
+    args.last.is_a?(::Hash) ? args.pop : {}
+  end
+
+  def validate_options(options)
     if options.key?(:instance_accessor) && (options.key?(:instance_reader) || options.key?(:instance_writer))
       raise InvalidOptionError, 'Invalid options passed to method'
     end
-    create_cattr_methods(*args, options)
   end
 
   def create_cattr_methods(*args, options)
     args.each do |attribute|
-      initialize_class_attr(attribute)
-      create_class_accessor_methods(attribute)
-      unless options[:instance_writer].eql?(false) || options[:instance_accessor].eql?(false)
-        create_instance_writer_method(attribute)
-      end
-      unless options[:instance_reader].eql?(false) || options[:instance_accessor].eql?(false)
-        create_instance_reader_method(attribute)
-      end
+      name_hash = {
+        getter_method_name: attribute.to_sym,
+        setter_method_name: "#{attribute}=".to_sym,
+        class_attribute: "@@#{attribute}".to_sym
+      }
+      initialize_class_attr(name_hash)
+      create_class_accessors(name_hash)
+      create_instance_accessors(name_hash, options)
     end
   end
 
-  def create_class_accessor_methods(attribute)
+  def create_class_accessors(name_hash)
     singleton_class.class_eval do
-      define_method("#{attribute}=") { |value| class_variable_set("@@#{attribute}", value) }
-      define_method("#{attribute}") { class_variable_get("@@#{attribute}") }
+      define_method(name_hash[:setter_method_name]) { |value| class_variable_set(name_hash[:class_attribute], value) }
+      define_method(name_hash[:getter_method_name]) { class_variable_get(name_hash[:class_attribute]) }
     end
   end
 
-  def initialize_class_attr(attribute)
-    class_eval %(@@#{attribute} = nil unless defined? @@#{attribute})
+  def create_instance_accessors(name_hash, options)
+    create_instance_writer_method(name_hash) unless check_for_options(options, :instance_writer)
+    create_instance_reader_method(name_hash) unless check_for_options(options, :instance_reader)
   end
 
-  def create_instance_writer_method(attribute)
-    class_eval do
-      define_method("#{attribute}=") do |value|
-        self.class.class_eval("@@#{attribute} = #{value}")
-      end
+  def check_for_options(options, method_to_check)
+    options[method_to_check].eql?(false) || options[:instance_accessor].eql?(false)
+  end
+
+  def initialize_class_attr(name_hash)
+    class_variable_set(name_hash[:class_attribute], nil)
+  end
+
+  def create_instance_writer_method(name_hash)
+    define_method(name_hash[:setter_method_name]) do |value|
+      self.class.class_variable_set(name_hash[:class_attribute], value)
     end
   end
 
-  def create_instance_reader_method(attribute)
-    class_eval do
-      define_method(attribute) do
-        self.class.class_eval("@@#{attribute}")
-      end
+  def create_instance_reader_method(name_hash)
+    define_method(name_hash[:getter_method_name]) do
+      self.class.class_variable_get(name_hash[:class_attribute])
     end
   end
 end
